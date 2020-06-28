1140760cd7e26eac9aae0a6a1edfed551cbec8a1
hironoriyh <hironoriyh@gmail.com>
2020-06-29 08:19:13 +0900

    grasshopper to npy file

---
+++ text2numpy.py
@@ -0,0 +1,106 @@
+# To add a new cell, type '# %%'
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import os
+
+
+fs = os.listdir("./test/tree3/")
+fs.sort()
+filenames = []
+for filename in fs:
+    # if(filename[-4:] == ".png"):
+    #     print(filename)
+    if("pattern_1" in filename):
+        print(filename, '_'.join(filename.split("_")[:4]))
+        filenames.append('_'.join(filename.split("_")[:4]))
+    else:
+        continue
+
+fs = os.listdir("data/")
+# fs.sort()
+print(fs)
+
+for filename in filenames:
+    size = 32
+    print(filename)
+    if(filename + "_all.npy" in fs): 
+        print(filename, " is already done!")
+        continue
+
+    try:
+        all_txt = open("test/tree3/" + filename + "_pattern_1.txt", 'r')
+        mask_txt = open("test/tree3/" + filename + "_pattern_2.txt", 'r')
+        # size = mask_txt.readline(0)
+        vox_all = all_txt.readlines()[1].split(",")
+        vox_mask = mask_txt.readlines()[1].split(",")
+    except:
+        print("failed to open" , filename)
+        continue
+
+    ## culled pattern_1
+    all_np = []
+    for vox in vox_all:
+        # print(i, vox)
+        if(vox== '0'):
+            all_np.append(0)
+        elif(vox=='1'):
+            all_np.append(1)
+        else:
+            print(vox)
+    all_np = np.array(all_np, dtype=np.uint8)
+    all_np = np.reshape(all_np, (size,size,size))
+
+    ## culled pattern_2
+    pattern_2 = []
+    for vox in vox_mask:
+        if(vox== '0'):
+            pattern_2.append(0)
+        elif(vox=='1'):
+            pattern_2.append(1)
+        else:
+            print(vox)
+    # print(len(pattern_2))
+
+    culled_idx = 0
+    culled_list = []
+    for n, i in enumerate(all_np.ravel()):
+        if(i == 1): 
+            culled_list.append(n)
+
+    culled_list_index = 0
+    masked_vol = all_np.ravel().copy()
+    # print(masked_vol.shape)
+    missing_parts = np.zeros(size**3)
+
+    for i, vox in enumerate(masked_vol):
+        if any(i == c for c in culled_list):
+            if(pattern_2[culled_list_index] == 1):
+                # print("missing parts" , i, "masked vol", masked_vol[i])
+                missing_parts[i] = 1
+                masked_vol[i] = 0
+            culled_list_index +=1
+
+    # print(missing_parts.shape, masked_vol.shape)
+    missing_parts = np.reshape(missing_parts, (size,size,size))
+    masked_vol = np.reshape(masked_vol, (size,size,size))
+    print("shapes of missing parts and masked_vol", missing_parts.shape, masked_vol.shape)
+
+    fig = plt.figure()
+    fig = plt.figure(figsize=plt.figaspect(0.3))
+    fig.suptitle(filename[0], fontsize=16)
+
+    ax1 = fig.add_subplot(131, title='all', projection='3d') 
+    ax2 = fig.add_subplot(132, title='missing', projection='3d')
+    ax3 = fig.add_subplot(133, title='masked volume', projection='3d') 
+    # ax1 = fig.gca(projection='3d')
+    ax1.voxels(all_np, facecolors='red', edgecolor='k')
+    ax2.voxels(missing_parts, facecolors='green', edgecolor='k')
+    ax3.voxels(masked_vol, facecolors='blue', edgecolor='k')
+    fig_path = "data/" + filename +".png"
+    plt.savefig(fig_path) # get only the filename
+    # plt.show()
+
+    np.save("data/" + filename + "_all.npy", all_np)
+    np.save("data/" + filename + "_missing.npy", missing_parts)
+    np.save("data/" + filename + "_masked.npy", masked_vol)
